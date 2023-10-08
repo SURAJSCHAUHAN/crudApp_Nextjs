20435ae81c3a589ad0d31538f3cf5b842f84ac5a
Chauhan Suraj Singh <surajsinghc423@gmail.com>
2023-10-08 12:29:20 +0530

    first commit

---
+++ components/TopicList.js
@@ -0,0 +1,38 @@
+import RemoveButton from "./RemoveButton"
+import Link from "next/link"
+import {HiPencilAlt} from "react-icons/hi"
+
+const getTopics = async()=>{
+try {
+    const res=await fetch('http://localhost:3000/api/topics',{cache:"no-store"});
+    if(!res.ok){
+        throw new Error("Failed to fetch topics");
+    }
+    return res.json();   
+} catch (error) {
+    console.log("Error loading topics: ",error);
+}
+};
+
+const TopicList = async() => {
+    const {topics}= await getTopics();
+  return (
+    <>
+    {topics.map((curntElem)=>(
+        <div key={curntElem._id} className="p-4 border border-slate-300 my-3 flex justify-between gap-5 items-start">
+            <div>
+                <h2 className="font-bold text-2xl">{curntElem.title}</h2>
+                <div>{curntElem.description}</div>
+            </div>
+            <div  className="flex gap-2">
+                <RemoveButton id={curntElem._id}/>
+                <Link href={`/editTopic/${curntElem._id}`}> <HiPencilAlt size={24}/> </Link>
+            </div>
+        </div>
+    ))}
+      
+    </>
+  )
+}
+
+export default TopicList
